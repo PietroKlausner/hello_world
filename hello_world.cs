1d019aa00ca490837ca9d7db2bd7be6667a1ca5e
Pietro Klausner <pietro.klausner@studenti.unimi.it>
2021-03-03 12:38:19 +0100

    A new nice addition

--- hello_world.cs
+++ hello_world.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 
+// UN CORDIALE SALUTO
 namespace _20210303
 {
     class hello_world
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length == 0) {
+                Console.WriteLine("Hello World!");
+            }
+            else {
+                string name = args[0];
+                Console.WriteLine("Hello, " + name);
+                }
+            
         }
     }
 }
