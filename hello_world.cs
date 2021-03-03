3e1be015d9366d9dd98b616fe9d12b931ce16b93
Pietro Klausner <pietro.klausner@studenti.unimi.it>
2021-03-03 12:20:57 +0100

    First commit

---
+++ hello_world.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _20210303
+{
+    class hello_world
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Wold!");
+        }
+    }
+}
